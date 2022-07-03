306ddc44d0e23db17851442189d10b11136cd5a3
Phaedrus Raznikov <praznikov@cerego.com>
2022-07-03 08:20:06 -0400

    Partial support for offline notes

--- local-storage.ts
+++ local-storage.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createBook } from "./firebase";
+import { createNote } from "./firebase";
 import { BookPayload, NotePayload, TBook, TNote, VocabPayload } from "./types";
 
 type ActionKey =
@@ -10,11 +10,14 @@ type ActionKey =
   | "delete note"
   | "update note";
 
-type ActionPayload = BookPayload | NotePayload | VocabPayload;
+type ActionPayload = {
+  id?: string;
+  body: BookPayload | NotePayload | VocabPayload;
+};
 
-export const UNFINISHED_BOOKS_KEY = "unfinished books"
-export const FINISHED_BOOKS_KEY = "finished books"
-export const UNREAD_BOOKS_KEY = "unread books"
+export const UNFINISHED_BOOKS_KEY = "unfinished books";
+export const FINISHED_BOOKS_KEY = "finished books";
+export const UNREAD_BOOKS_KEY = "unread books";
 export const NOTES_KEY = "notes";
 
 export const ACTIONS = {
@@ -35,55 +38,76 @@ export const clearStorage = async (): Promise<Array<void>> => {
   return Promise.all(promises);
 };
 
-export const setLocalUnfinishedBooks = async (books: Array<TBook>): Promise<void> => {
-  return await AsyncStorage.setItem(UNFINISHED_BOOKS_KEY, JSON.stringify(books))
-}
-
-export const getLocalUnfinishedBooks = async (): Promise<Array<TBook>> => {
-  const books = await AsyncStorage.getItem(UNFINISHED_BOOKS_KEY)
-  return JSON.parse(books || "")
-}
-
-export const setLocalFinishedBooks = async (books: Array<TBook>): Promise<void> => {
-  return await AsyncStorage.setItem(FINISHED_BOOKS_KEY, JSON.stringify(books))
-}
-
-export const getLocalFinishedBooks = async (): Promise<Array<TBook>> => {
-  const books = await AsyncStorage.getItem(FINISHED_BOOKS_KEY)
-  return JSON.parse(books || "")
-}
-
-export const setLocalUnreadBooks = async (books: Array<TBook>): Promise<void> => {
-  return await AsyncStorage.setItem(UNREAD_BOOKS_KEY, JSON.stringify(books))
-}
-
-export const getLocalUnreadBooks = async (): Promise<Array<TBook>> => {
-  const books = await AsyncStorage.getItem(UNREAD_BOOKS_KEY)
-  return JSON.parse(books || "")
-}
-
-export const setLocalNotes = async (notes: Array<TNote>): Promise<void> => {
-  return await AsyncStorage.setItem(NOTES_KEY, JSON.stringify(notes));
+export const setLocalUnfinishedBooks = async (
+  books: Array<TBook>
+): Promise<void> => {
+  return await AsyncStorage.setItem(
+    UNFINISHED_BOOKS_KEY,
+    JSON.stringify(books)
+  );
 };
 
-export const getLocalNotes = async (): Promise<Array<TNote>> => {
-  const notes = await AsyncStorage.getItem(NOTES_KEY);
+export const getLocalUnfinishedBooks = async (): Promise<Array<TBook>> => {
+  const books = await AsyncStorage.getItem(UNFINISHED_BOOKS_KEY);
+  return JSON.parse(books || "");
+};
+
+export const setLocalFinishedBooks = async (
+  books: Array<TBook>
+): Promise<void> => {
+  return await AsyncStorage.setItem(FINISHED_BOOKS_KEY, JSON.stringify(books));
+};
+
+export const getLocalFinishedBooks = async (): Promise<Array<TBook>> => {
+  const books = await AsyncStorage.getItem(FINISHED_BOOKS_KEY);
+  return JSON.parse(books || "");
+};
+
+export const setLocalUnreadBooks = async (
+  books: Array<TBook>
+): Promise<void> => {
+  return await AsyncStorage.setItem(UNREAD_BOOKS_KEY, JSON.stringify(books));
+};
+
+export const getLocalUnreadBooks = async (): Promise<Array<TBook>> => {
+  const books = await AsyncStorage.getItem(UNREAD_BOOKS_KEY);
+  return JSON.parse(books || "");
+};
+
+export const setLocalNotes = async (
+  bookId: string,
+  notes: Array<TNote>
+): Promise<void> => {
+  return await AsyncStorage.setItem(
+    `${bookId} ${NOTES_KEY}`,
+    JSON.stringify(notes)
+  );
+};
+
+export const getLocalNotes = async (bookId: string): Promise<Array<TNote>> => {
+  const notes = await AsyncStorage.getItem(`${bookId} ${NOTES_KEY}`);
   return JSON.parse(notes || "");
 };
 
+const getActions = async (key: ActionKey): Promise<Array<ActionPayload>> => {
+  const actions = await AsyncStorage.getItem(key);
+  return JSON.parse(actions || "[]");
+};
+
+// TODO: call when create note action is attempted while offline
+// TODO: create note in local storage
+// TODO: disable UI for actions without offline support
 export const queueAction = async (
   key: ActionKey,
   payload: ActionPayload
 ): Promise<void> => {
   // Get queued actions
-  let actions = await AsyncStorage.getItem(key);
-  // Parse them
-  actions = JSON.parse(actions);
+  let actions: Array<ActionPayload> = await getActions(key);
   // Queue new action
   const newAction = payload;
-  actions = actions.push(newAction);
+  actions.push(newAction);
   // Set stringified actions queue
-  return await AsyncStorage.setItem(key, actions);
+  return await AsyncStorage.setItem(key, JSON.stringify(actions));
 };
 
 export const processAction = async (
@@ -95,12 +119,13 @@ export const processAction = async (
   // We want to keep the action in the queue if it fails
   try {
     switch (key) {
-      case "create book":
-        await createBook(payload as BookPayload);
+      case "create note":
+        if (!payload.id) {
+          throw new Error("CREATE_NOTE action requires id attr on payload");
+        }
+        await createNote(payload.id, payload.body as NotePayload);
         break;
       // TODO: Add handling for each case
-      // TODO: Refactor payload types and firebase function parameters
-      //       Need to take id as attr on payload
       default:
         console.error(`Cannot process unrecognized action key: ${key}`);
         throw new Error(`Cannot process unrecognized action key: ${key}`);
@@ -111,35 +136,24 @@ export const processAction = async (
   }
 
   // If the Firebase creation succeeds, remove the action from local storage
-  let actions = await AsyncStorage.getItem(key);
-  actions = JSON.parse(actions);
+  let actions: Array<ActionPayload> = await getActions(key);
   actions = actions.filter(
     (action: any) => JSON.stringify(action) !== JSON.stringify(payload)
   );
-  return await AsyncStorage.setItem(key, actions);
+  return await AsyncStorage.setItem(key, JSON.stringify(actions));
 };
 
 // TODO: Define process for processing queue
 //       Perhaps useEffect with netInfo in dep array?
 //       Whenever we go from offline to online, process actions queue
-export const processCreateBookAction = async (
-  createBookAction: BookPayload
-): Promise<void> => {
-  // Create the book in Firebase
-  // Throw an error and halt execution if creation fails
-  // We want to keep the action in the queue if it fails
+export const processActions = async (key: ActionKey): Promise<void> => {
   try {
-    await createBook(createBookAction);
+    const actionsStr: string | null = await AsyncStorage.getItem(key);
+    const actions = JSON.parse(actionsStr || "[]");
+    // TODO: Verify that this works fine when some actions fail to process
+    actions.forEach((action: any) => processAction(key, action));
   } catch (err) {
     console.error(err);
     throw new Error(err);
   }
-
-  // If the Firebase creation succeeds, remove the action from local storage
-  let actions = await AsyncStorage.getItem(ACTIONS.CREATE_BOOK);
-  actions = JSON.parse(actions);
-  actions = actions.filter(
-    (action: any) => JSON.stringify(action) !== JSON.stringify(createBookAction)
-  );
-  return await AsyncStorage.setItem(ACTIONS.CREATE_BOOK, actions);
 };
